732017c6c0c1801377716bd839d9c2a14e693438
jpinto <abcd@efgh>
2017-06-13 17:43:46 +1000

    Added OpenGL surface to library.

---
+++ app/src/main/salvador-lib/OpenGLSurface.cpp
@@ -0,0 +1,13 @@
+//
+// Created by jp on 13/06/2017.
+//
+
+#include "OpenGLSurface.h"
+
+OpenGLSurface::OpenGLSurface()
+{
+}
+
+OpenGLSurface::~OpenGLSurface()
+{
+}
---
+++ app/src/main/salvador-lib/OpenGLSurface.h
@@ -0,0 +1,15 @@
+//
+// Created by jp on 13/06/2017.
+// Description: Responds to OpenGL surface events.
+//
+
+#ifndef VOLUMERENDERER_OPENGLSURFACE_H
+#define VOLUMERENDERER_OPENGLSURFACE_H
+
+class OpenGLSurface
+{
+    OpenGLSurface();
+    ~OpenGLSurface();
+};
+
+#endif //VOLUMERENDERER_OPENGLSURFACE_H
