ceb88e195e9e53425a1db26e678e4bd4c68f4685
jpinto <abcd@efgh>
2017-06-14 19:50:19 +1000

    Modified CMake build system to recursively include all required source files. Also added OpenGL and logger libraries from the NDK.

--- app/src/main/salvador-lib/JNI.cpp
+++ app/src/main/salvador-lib/JNI.cpp
@@ -1,9 +1,16 @@
 #include <jni.h>
 #include <string>
 
+// NOTE: Function definitions must be enclosed within the 'extern' keyword to prevent the compiler
+// from mangling the function name. Without this declaration, JNI will not be able to link to these
+// functions at runtime.
+
 extern "C"
-JNIEXPORT jstring JNICALL
-Java_jp_volumerenderer_SalvadorLib_stringFromJNI(JNIEnv *env, jobject)
+{
+    JNIEXPORT jstring JNICALL Java_jp_volumerenderer_SalvadorLib_stringFromJNI(JNIEnv *env, jobject);
+};
+
+JNIEXPORT jstring JNICALL Java_jp_volumerenderer_SalvadorLib_stringFromJNI(JNIEnv *env, jobject)
 {
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
