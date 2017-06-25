b9ff7bce6d1af1f9230f2c74f9d811ab5ea9ae4e
jpinto <abcd@efgh>
2017-06-25 23:03:31 +1000

    Removed emulator support and unnecessary JNI function call.

--- app/src/main/java/jp/volumerenderer/MainActivity.java
+++ app/src/main/java/jp/volumerenderer/MainActivity.java
@@ -7,23 +7,11 @@ import android.opengl.GLSurfaceView;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
 {
     private GLSurfaceView glSurfaceView;
-    boolean rendererSet = false;
-
-    private boolean emulatorDetected()
-    {
-        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1
-                && (Build.FINGERPRINT.startsWith("generic")
-                || Build.FINGERPRINT.startsWith("unknown")
-                || Build.MODEL.contains("google_sdk")
-                || Build.MODEL.contains("Emulator")
-                || Build.MODEL.contains("Android SDK built for x86"));
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -31,26 +19,15 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // TODO: Remove! Example of a call to a native method
-        Toast.makeText(this, SalvadorLib.stringFromJNI(), Toast.LENGTH_LONG).show();
-
         ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
         ConfigurationInfo configurationInfo = activityManager.getDeviceConfigurationInfo();
-        final boolean supportsEs3 = configurationInfo.reqGlEsVersion >= 0x30000 || emulatorDetected();
 
-        if (supportsEs3)
+        if (configurationInfo.reqGlEsVersion >= 0x30000 )
         {
             glSurfaceView = new GLSurfaceView(this);
-
-            if (emulatorDetected())
-            {
-                // Avoids crashes on startup with some emulator images.
-                glSurfaceView.setEGLConfigChooser(8, 8, 8, 8, 16, 0);
-            }
-
+            glSurfaceView.setEGLConfigChooser(8, 8, 8, 8, 16, 0);
             glSurfaceView.setEGLContextClientVersion(3);
             glSurfaceView.setRenderer(new RendererWrapper());
-            rendererSet = true;
             setContentView(glSurfaceView);
         }
         else
@@ -65,7 +42,7 @@ public class MainActivity extends AppCompatActivity
     {
         super.onPause();
 
-        if (rendererSet)
+        if (glSurfaceView != null)
         {
             glSurfaceView.onPause();
         }
@@ -76,7 +53,7 @@ public class MainActivity extends AppCompatActivity
     {
         super.onResume();
 
-        if (rendererSet)
+        if (glSurfaceView != null)
         {
             glSurfaceView.onResume();
         }
