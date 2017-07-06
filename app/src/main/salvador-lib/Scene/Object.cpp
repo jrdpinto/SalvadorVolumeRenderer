560e9b69c3514c2c422d73de8729e97788f639a3
jpinto <abcd@efgh>
2017-07-06 16:24:12 +1000

    Added a new object class to represent scene nodes.

--- app/src/main/salvador-lib/Scene/Object.cpp
+++ app/src/main/salvador-lib/Scene/Object.cpp
@@ -3,3 +3,55 @@
 //
 
 #include "Object.h"
+
+Object::Object() : state_(State::STATIONARY), transformationUpdateRequired_(true)
+{
+    pos_ = {0.0f, 0.0f, 0.0f};
+    orientation_ = {0.0f, 0.0f, 0.0f};
+    mat4x4_identity(transformationMatrix_);
+}
+
+Object::~Object()
+{
+}
+
+void Object::setPos(float x, float y, float z)
+{
+    pos_ = {x,y,z};
+    transformationUpdateRequired_ = true;
+}
+
+void Object::setOrientation(float pitch, float yaw, float roll)
+{
+    orientation_ = {pitch,yaw,roll};
+    transformationUpdateRequired_ = true;
+}
+
+void Object::update(float dt)
+{
+    if (state_ == State::MOVING)
+    {
+        // Update object position
+    }
+
+    updateTransformation();
+}
+
+void Object::updateTransformation()
+{
+    if (transformationUpdateRequired_)
+    {
+        // TODO: !!!
+
+        // Rotate object around origin using quaternions
+
+        // Translate object to current position
+
+        transformationUpdateRequired_ = false;
+    }
+}
+
+const mat4x4 *Object::getViewMatrix() const
+{
+    return (mat4x4 const *) &transformationMatrix_;
+}
--- app/src/main/salvador-lib/Scene/Object.h
+++ app/src/main/salvador-lib/Scene/Object.h
@@ -1,16 +1,46 @@
 //
 // Created by jp on 6/07/2017.
+// A high level class that defines an object in the scene. All objects have a position and an
+// orientation. Objects can also respond to touch events/gestures.
 //
 
-#ifndef VOLUMERENDERER_OBJECT_H
-#define VOLUMERENDERER_OBJECT_H
+#ifndef SALVADOR_OBJECT_H
+#define SALVADOR_OBJECT_H
 
+#include <array>
 
+#include "../external/linmath/linmath.h"
 
-class Object {
+class Object
+{
+    enum class State
+    {
+        STATIONARY,
+        MOVING
+    };
+    State state_;
 
+    // X, Y, Z
+    std::array<float, 3> pos_;
+
+    // X: Pitch     Y: Yaw      Z: Roll
+    std::array<float, 3> orientation_;
+
+    mat4x4 transformationMatrix_;
+
+    bool transformationUpdateRequired_;
+
+    void updateTransformation();
+public:
+    Object();
+    virtual ~Object();
+
+    const mat4x4* getViewMatrix() const;
+
+    virtual void update(float dt);
+
+    virtual void setPos(float x, float y, float z);
+    virtual void setOrientation(float pitch, float yaw, float roll);
 };
 
-
-
-#endif //VOLUMERENDERER_OBJECT_H
+#endif //SALVADOR_OBJECT_H
