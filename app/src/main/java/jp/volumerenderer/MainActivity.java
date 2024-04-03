69328cee85f49ca9e1e83238c7e9257be85be8af
jp <jpgitpublic@gmail.com>
2024-04-03 18:12:18 +1100

    Fixed build issues on Android 33.

--- app/src/main/java/jp/volumerenderer/MainActivity.java
+++ app/src/main/java/jp/volumerenderer/MainActivity.java
@@ -6,7 +6,7 @@ import android.content.pm.ConfigurationInfo;
 import android.content.res.AssetManager;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.WindowManager;
 import android.widget.Toast;
 
